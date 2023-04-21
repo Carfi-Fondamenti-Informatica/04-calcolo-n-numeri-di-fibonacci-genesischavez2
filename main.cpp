fe0a5a8d1af2b8862629d4eaf5812c4b4c79f88e
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-04-21 20:36:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   int n,m,b,c,d;
+   cin >>n;
+   if(n>=2) {
+      m=n;
+      b=n+m;
+      c=m+b;
+      d=b+c;
+      cout << m << endl;
+      cout << b << endl;
+      cout << c << endl;
+      cout << d << endl;
+      
+   }      
+   else {
+      cout << "errore";
+   }   
+   
    return 0;
 }
